7c339f70ab4a831d039137be8245accc27cb6c23
YCK1204 <dpcks5959@naver.com>
2025-07-29 20:18:53 +0900

    [FEAT] Item ScriptableObject 데이터화

--- Archero/Assets/Scripts/Item/Item.cs
+++ Archero/Assets/Scripts/Item/Item.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public BaseItemData itemData;
-    Animator _animator => itemData.Animator;
-    Sprite _icon => itemData.Icon;
-    string _name => itemData.Name;
-    ItemType _type => itemData.Type;
-    public void Use()
+    BaseItemData itemData;
+    public Animator Animator => itemData.Animator;
+    public Sprite Icon => itemData.Icon;
+    public string Name => itemData.Name;
+    ItemType Type => itemData.Type;
+    public Item(BaseItemData data)
     {
-        switch (_type)
+        itemData = data;
+    }
+    /// <summary>
+    /// 아이템을 타입에 따라 사용합니다.
+    /// </summary>
+    /// <param name = "player">플레이어</param>
+    public void Use(/*Player player*/)
+    {
+        switch (Type)
         {
             case ItemType.Collectable:
                 CollectableItemData collectableItem = itemData as CollectableItemData;
                 collectableItem.Collect();
+                //collectableItem.Collect(player);
                 break;
             case ItemType.Equippable:
                 EquippableItemData equippableItem = itemData as EquippableItemData;

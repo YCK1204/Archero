daeb09f35ca395fbb332d053fd0a80eaccff063a
LeeYoungSin <dldudtls100@gmail.com>
2025-07-30 16:49:41 +0900

    [FIX] 스킬데이터도 입력받게 RewardManager 수정

--- Archero/Assets/WorkSpase/Lee/Scripts/Data/Base/RewardData.cs
+++ Archero/Assets/WorkSpase/Lee/Scripts/Data/Base/RewardData.cs
@@ -4,26 +4,19 @@ using UnityEngine;
 
 namespace Lee.Scripts
 {
-    public enum RewardType
-    {
-        Stat,
-        Skill,
-    }
-
-
-    [CreateAssetMenu(fileName = "RewardData", menuName = "Reward/Reward Data")]
     public class RewardData : ScriptableObject
     {
-        [Header("타입설정")]
-        public RewardType type;
         [Header("Prefab 연결")]
         public GameObject prefab;
+
         [Header("이름")]
         public string rewarName;
+
         [Header("(선택)보상 설명")]
         [TextArea] public string description;  // (선택) 보상 설명
+
+        [Header("기본 드롭 가중치")]
+        [Tooltip("값이 클수록 뽑힐 확률이 높아짐")]
         public float baseWeight = 1f;
-
     }
-
 }
--- Archero/Assets/WorkSpase/Lee/Scripts/UI/RewardSelect_PopUpUI.cs
+++ Archero/Assets/WorkSpase/Lee/Scripts/UI/RewardSelect_PopUpUI.cs
@@ -18,66 +18,19 @@ namespace Lee.Scripts
 
         private void OnDisable()
         {
-            StartCoroutine(StartRountine());
+            StartCoroutine(OpenRountine());
         }
 
-        IEnumerator StartRountine()
+        IEnumerator OpenRountine()
         {
             yield return new WaitForSeconds(0.2f);
             curRewardList.Add(rectTransform["RewardSlot1"]);
             curRewardList.Add(rectTransform["RewardSlot2"]);
             curRewardList.Add(rectTransform["RewardSlot3"]);
             yield return new WaitForSeconds(0.2f);
-            RandomReward();
+            var picks = GameManager.Reward.GetRandomRewards(curRewardList.Count);
+
         }
-
-        private void RandomReward()
-        {
-            // 전체 섞기
-            var allTypes = GameManager.Reward.AllRewardTypes.ToList();
-            Shuffle(allTypes);
-            var picks = allTypes.Take(curRewardList.Count).ToArray();
-            for (int i = 0; i < 3; i++)
-            {
-                var slotBtn = curRewardList[i];
-                var slotTf = slotBtn.transform as RectTransform;
-
-                foreach (Transform child in slotTf)
-                    Destroy(child.gameObject);
-
-
-                var data = GameManager.Reward.GetData(picks[i]);
-                var go = GameManager.Resource.Instantiate(data.prefab);
-
-  
-                go.transform.SetParent(slotTf, false);// 로컬 포지션
-
-                if (go.TryGetComponent<RectTransform>(out var rt))
-                {
-                    rt.position = slotTf.position;
-                }
-            }
-        }
-
-        private void Shuffle<T>(IList<T> list)
-        {
-            for (int i = list.Count - 1; i > 0; i--)
-            {
-                int j = Random.Range(0, i + 1);
-                (list[i], list[j]) = (list[j], list[i]);
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }

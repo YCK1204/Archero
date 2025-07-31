38dca0f74f796a7122aef89306cf31014f311180
Heoyoon <parechae@gmail.com>
2025-07-31 18:06:04 +0900

    [feat] 탄막 관련 스크립트 추가

---
+++ Archero/Assets/Yoon/Handler/IBarrages/Barrages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handler.Barrages
+{
+    public abstract class Barrages
+    {
+        protected float shotDelay;//사격 딜레이
+        protected float shotDuration;//사격 지속시간
+        protected float angleGap;//z축을 기준
+        public int shotCount;//사격 횟수
+        public Barrages(float shotDelay, float shotDuration, float angleGap, float poviotAngle, int shotCount)
+        {
+            this.shotDelay = shotDelay;
+            this.shotDuration = shotDuration;
+            this.angleGap = angleGap;
+            this.shotCount = shotCount;
+        }
+
+        public abstract void OnShot();
+    }
+}
---
+++ Archero/Assets/Yoon/Handler/IBarrages/MultiShot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handler.Barrages
+{
+    public class MultiShot : Barrages
+    {
+        float startAngle;
+        public MultiShot(float shotDelay, float shotDuration, float angleGap, float pivotAngle) : base(shotDelay, shotDuration, angleGap, pivotAngle)
+        {
+            this.shotDelay = shotDelay;
+            this.shotDuration = shotDuration;
+            this.angleGap = angleGap;
+            
+            startAngle = 
+        }
+
+        public override void OnShot()
+        {
+            
+        }
+    }
+}

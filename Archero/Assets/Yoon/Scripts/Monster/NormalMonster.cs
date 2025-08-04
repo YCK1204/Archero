e2a081d333a6a173d5e0baf001ca68854cedc754
Heoyoon <parechae@gmail.com>
2025-08-04 23:10:55 +0900

    [chore] gizmo 전처리기 추가

--- Archero/Assets/Yoon/Scripts/Monster/NormalMonster.cs
+++ Archero/Assets/Yoon/Scripts/Monster/NormalMonster.cs
@@ -49,7 +49,7 @@ public class NormalMonster : Monster
     }
 
 
-
+#if UNITY_EDITOR
     void OnDrawGizmosSelected()
     {
         if (patrolPositions == null || patrolPositions.Length <= 0) return;
@@ -59,6 +59,8 @@ public class NormalMonster : Monster
             Gizmos.DrawSphere(patrolPositions[i], 0.5f);
         }
     }
+#endif
+
 
     public override void Spawn(MobType type, Vector3[] patrolPos, MonsterStat stat, ChessCharType chessType)
     {

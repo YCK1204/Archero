b2b0ee2c1346554524985e143cc3a1ef5049f199
YCK1204 <dpcks5959@naver.com>
2025-08-05 01:30:55 +0900

    [FEAT] Door Init Handler

--- Archero/Assets/Scripts/Door.cs
+++ Archero/Assets/Scripts/Door.cs
@@ -6,12 +6,41 @@ public class Door : MonoBehaviour
 {
     [SerializeField]
     Collider2D Collider2D;
-    public void SetTriggerPosition(Vector2Int direction)
+    [SerializeField]
+    Sprite VerticalCloseSprite;
+    [SerializeField]
+    Sprite VerticalOpenSprite;
+    [SerializeField]
+    Sprite HorizontalCloseSprite;
+    [SerializeField]
+    Sprite HorizontalOpenSprite;
+    public void Init(GameObject parent, Vector2Int startPos, Vector2Int endPos, bool start)
     {
-        Collider2D.transform.position = transform.position + (Vector3Int)direction;
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // Event
+        var renderer = GetComponent<SpriteRenderer>();
+        Vector3 offset = new Vector3(0.5f, 0.5f, 0);
+        bool horizontal = (endPos - startPos).x != 0;
+
+        Vector2Int basePos = start ? startPos : endPos;
+        transform.position = (Vector3Int)basePos + offset;
+
+        if (horizontal)
+        {
+            renderer.sprite = HorizontalCloseSprite;
+
+            bool leftToRight = startPos.x < endPos.x;
+            bool placeLeft = (start && leftToRight) || (!start && !leftToRight);
+
+            Collider2D.transform.position = transform.position + (placeLeft ? Vector3.left : Vector3.right);
+            renderer.flipX = !placeLeft;
+        }
+        else
+        {
+            renderer.sprite = VerticalCloseSprite;
+
+            bool bottomToTop = startPos.y < endPos.y;
+            bool placeBottom = (start && bottomToTop) || (!start && !bottomToTop);
+
+            Collider2D.transform.position = transform.position + (placeBottom ? Vector3.down : Vector3.up);
+        }
     }
 }
